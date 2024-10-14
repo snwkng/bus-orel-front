2be32c18c5ea38d7be15c11e09403d7d59f66d66
snwkng <dmpetlaj@gmail.com>
2024-10-15 01:12:25 +0300

    pm2 config update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
 	apps: [
 		{
-			name: 'bus-orel-front',
+			name: 'front',
 			port: '3000',
 			exec_mode: 'cluster',
 			instances: 'max',
 			script: './.output/server/index.mjs',
-			exp_backoff_restart_delay: 100
+			exp_backoff_restart_delay: 100,
+			watch: true
 		}
 	]
 }
